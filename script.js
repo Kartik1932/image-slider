a141f341470f9092f28e4c88bd8c70ada4d1e9a8
kartik <kartiknangla882@gmail.com>
2025-08-27 09:47:30 +0530

    Added dots at the bottom for easier navigation

--- script.js
+++ script.js
@@ -2,11 +2,29 @@ const carouselImages = document.querySelector(".carousel-images");
 const images = document.querySelectorAll(".carousel-images img");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
+const dotsContainer = document.querySelector(".dots");
 
 let index = 0;
 
+images.forEach((_, i) => {
+    const dot = document.createElement("div");
+    dot.classList.add("dot")
+    if(i === 0) dot.classList.add("active");
+    dot.addEventListener("click", () => {
+        index = i;
+        showImage();
+    })
+
+    dotsContainer.appendChild(dot);
+})
+
+const dots = document.querySelectorAll(".dot");
+
 function showImage(){
     carouselImages.style.transform = `translateX(${-index * 600}px)`;
+
+    dots.forEach(dot => dot.classList.remove("active"));
+    dots[index].classList.add("active");
 }
 
 nextBtn.addEventListener("click", () => {
